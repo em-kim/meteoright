2301feef33db3f4b127b358c58cca4938e312ba2
bbalconi <bbalconi@gmail.com>
2017-09-18 16:06:52 -0600

    Ohhhhhhh boy.

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require("express");
- var app = express();
+var app = express();
 app.use(express.static('public'))
 
- /* serves main page */
- app.get("/", function(req, res) {
-    res.sendfile('index.htm')
- });
+/* serves main page */
+app.get("/", function(req, res) {
+    res.sendfile('./templates/index.htm')
+});
 
- var port = process.env.PORT || 5000;
- app.listen(port, function() {
-   console.log("Listening on " + port);
- });
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
