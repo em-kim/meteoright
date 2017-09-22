f5d626e943c42856e6c692fcdd4f916e00143bf9
KB-Spanish-Castle <kylegrantb@gmail.com>
2017-09-22 15:20:57 -0600

    added doggos

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var express = require("express");
 var app = express();
-app.use(express.static('public'))
+var path = require('path');
+app.use(express.static('public'));
 
-
- /* serves main page */
+//app.use(express.static(path.join(__dirname, './meteoright')));
+ /* serves main page
  app.get("/", function(req, res) {
     res.sendfile('./templates/index.htm')
+ });*/
+ app.get("/spaceDog", function(req, res) {
+    res.sendfile('./templates/spaceDog.html')
  });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
